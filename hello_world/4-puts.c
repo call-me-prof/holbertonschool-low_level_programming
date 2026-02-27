036f7c6158a60c5d8b81a465fcf39855ea398686
call-me-prof <OpFaisal4@hotmail.com>
2026-02-27 20:46:11 +0000

    Add task 4: Hello puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints a specific string using puts
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
