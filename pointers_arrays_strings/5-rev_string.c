fbd941797554159eaffebe7ef335867ec21eccd4
call-me-prof <OpFaisal4@hotmail.com>
2026-03-07 16:54:03 +0000

    Fix task 4 and add task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int i = 0;
+	char temp;
+
+	while (s[len] != '\0')
+		len++;
+
+	len--;
+	while (i < len)
+	{
+		temp = s[i];
+		s[i] = s[len];
+		s[len] = temp;
+		i++;
+		len--;
+	}
+}
