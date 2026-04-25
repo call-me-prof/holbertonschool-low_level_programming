07d84d3fe325f088b3158770f445196dea0cd967
call-me-prof <OpFaisal4@hotmail.com>
2026-04-25 15:00:08 +0000

    First task: malloc_checked function

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory to allocate
+ *
+ * Return: pointer to the allocated memory
+ * if malloc fails, cause normal process termination with status value of 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
